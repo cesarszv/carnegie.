a00f66790ef47478e8815600f71a6b3815476d20
cesarszv <cesar.zambrana.v@ucb.edu.bo>
2026-06-22 18:16:13 -0400

    fix: Set site and base configuration for GitHub Pages subfolder

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cesarszv.github.io',
+  base: '/carnegie./',
   build: {
     format: 'file'
   }
